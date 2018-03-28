efefdd45d1d3ac7f7cb82527a138131073531dc8
Shun Yao <ysmiracle@hotmail.com>
2018-03-28 06:07:19 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,15 @@
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    string = "The line is currently: "
+    katz_deli.each_with_index { |name, index|
+      string += "#{index+1}. #{name} "
+    }
+    puts string.chop!
+  end
+end
+
 def take_a_number(katz_deli, newCustomer)
   katz_deli << newCustomer
   puts "Welcome, #{newCustomer}. You are number #{katz_deli.length} in line."
@@ -9,17 +21,5 @@ def now_serving(katz_deli)
   else
     name = katz_deli.shift;
     puts "Currently serving #{name}."
-  end
-end
-
-def line(katz_deli)
-  if katz_deli.length == 0
-    puts "The line is currently empty."
-  else
-    string = "The line is currently: "
-    katz_deli.each_with_index { |name, index|
-      string += "#{index+1}. #{name} "
-    }
-    puts string.chop!
   end
 end
