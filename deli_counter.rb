000aac6a3224cdf88bd25bb5a687a66859ac97ee
Shun Yao <ysmiracle@hotmail.com>
2018-03-28 05:51:09 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,14 +13,13 @@ def nowServing(katz_deli)
 end
 
 def line(katz_deli)
-  string = "The line is currently: "
-  
   if katz_deli.length == 0
-    "The line is currently empty.";
+    "The line is currently empty."
   else
+    string = "The line is currently: "
     katz_deli.each_with_index { |name, index|
       string += "#{index+1}. #{name}"
     }
+    string
   end
-  string
 end
